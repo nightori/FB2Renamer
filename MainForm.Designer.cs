95e12d989e0fff47ddcbd67e3c19d73e20b3f9bc
Sergey <nightori@yandex.ru>
2020-02-16 01:53:55 +0600

    Initial commit!

---
+++ MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace FB2Renamer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxName2 = new System.Windows.Forms.TextBox();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.labelFilename = new System.Windows.Forms.Label();
+            this.buttonRename = new System.Windows.Forms.Button();
+            this.textBoxName1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBoxName2
+            // 
+            this.textBoxName2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxName2.Location = new System.Drawing.Point(134, 38);
+            this.textBoxName2.Name = "textBoxName2";
+            this.textBoxName2.Size = new System.Drawing.Size(219, 20);
+            this.textBoxName2.TabIndex = 4;
+            this.textBoxName2.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTitle.Location = new System.Drawing.Point(134, 64);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(219, 20);
+            this.textBoxTitle.TabIndex = 6;
+            this.textBoxTitle.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // labelFilename
+            // 
+            this.labelFilename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFilename.Location = new System.Drawing.Point(15, 87);
+            this.labelFilename.Name = "labelFilename";
+            this.labelFilename.Size = new System.Drawing.Size(338, 18);
+            this.labelFilename.TabIndex = 7;
+            this.labelFilename.Text = "(новое имя файла)";
+            this.labelFilename.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonRename
+            // 
+            this.buttonRename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRename.Location = new System.Drawing.Point(12, 108);
+            this.buttonRename.Name = "buttonRename";
+            this.buttonRename.Size = new System.Drawing.Size(341, 23);
+            this.buttonRename.TabIndex = 0;
+            this.buttonRename.Text = "Переименовать";
+            this.buttonRename.UseVisualStyleBackColor = true;
+            this.buttonRename.Click += new System.EventHandler(this.buttonRename_Click);
+            // 
+            // textBoxName1
+            // 
+            this.textBoxName1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxName1.Enabled = false;
+            this.textBoxName1.Location = new System.Drawing.Point(134, 13);
+            this.textBoxName1.Name = "textBoxName1";
+            this.textBoxName1.Size = new System.Drawing.Size(219, 20);
+            this.textBoxName1.TabIndex = 2;
+            this.textBoxName1.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(116, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Имя автора";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(12, 38);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(116, 21);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Фамилия автора";
+            this.checkBox2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Click += new System.EventHandler(this.checkBox_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(12, 64);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(116, 21);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Название книги";
+            this.checkBox3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.Click += new System.EventHandler(this.checkBox_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 141);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBoxName1);
+            this.Controls.Add(this.buttonRename);
+            this.Controls.Add(this.labelFilename);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.textBoxName2);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Переименование книги";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxName2;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.Label labelFilename;
+        private System.Windows.Forms.Button buttonRename;
+        private System.Windows.Forms.TextBox textBoxName1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace FB2Renamer
+{
+    public partial class MainForm : Form
+    {
+        string bookPath;
+        public MainForm()
+        {
+            InitializeComponent();
+            string[] arg = Environment.GetCommandLineArgs();
+            if (arg.Length == 1)
+            {
+                MessageBox.Show("Не указана книга для переименования!", "Ошибка");
+                Environment.Exit(0);
+            }
+            else
+            {
+                bookPath = arg[1];
+            }
+            getBookInfo();
+        }
+
+        private void getBookInfo()
+        {
+            XmlDocument book = new XmlDocument();
+            book.Load(bookPath);
+            XmlNamespaceManager xmlnm = new XmlNamespaceManager(book.NameTable);
+            xmlnm.AddNamespace("book", book.DocumentElement.NamespaceURI);
+            textBoxName1.Text = book.SelectSingleNode("//book:first-name", xmlnm).InnerText;
+            textBoxName2.Text = book.SelectSingleNode("//book:last-name", xmlnm).InnerText;
+            textBoxTitle.Text = book.SelectSingleNode("//book:book-title", xmlnm).InnerText;
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            string fn = "";
+            string bin = (checkBox1.Checked?"1":"0")+(checkBox2.Checked?"1":"0")+(checkBox3.Checked?"1":"0");
+            string[] str = new string[] { textBoxName1.Text, textBoxName2.Text, textBoxTitle.Text };
+            switch (bin)
+            {
+                case "111": fn = str[0] + " " + str[1] + " - " + str[2]; break;
+                case "110": fn = str[0] + " " + str[1]; break;
+                case "101": fn = str[0] + " - " + str[2]; break;
+                case "100": fn = str[0]; break;
+                case "011": fn = str[1] + " - " + str[2]; break;
+                case "010": fn = str[1]; break;
+                case "001": fn = str[2]; break;
+            }
+            if (fn.Length == 0)
+            {
+                labelFilename.Text = "";
+                buttonRename.Enabled = false;
+            }
+            else
+            {
+                labelFilename.Text = fn + ".fb2";
+                buttonRename.Enabled = true;
+            }
+        }
+
+        private void buttonRename_Click(object sender, EventArgs e)
+        {
+            FileInfo file = new FileInfo(bookPath);
+            string newPath = file.Directory.ToString() + "\\" + labelFilename.Text;
+            try
+            {
+                File.Move(bookPath, newPath);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка переменования. Например, это может быть вызвано наличием недопустимых символов в новом имени файла.","Ошибка!");
+                return;
+            }
+            Application.Exit();
+        }
+
+        private void checkBox_Click(object sender, EventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+            switch (checkBox.Name)
+            {
+                case "checkBox1": textBoxName1.Enabled = !textBoxName1.Enabled; break;
+                case "checkBox2": textBoxName2.Enabled = !textBoxName2.Enabled; break;
+                case "checkBox3": textBoxTitle.Enabled = !textBoxTitle.Enabled; break;
+            }
+            textBox_TextChanged(sender, e);
+        }
+    }
+}
